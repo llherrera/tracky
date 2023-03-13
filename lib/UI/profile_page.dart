a8ee4cf76a286538846ddfe0078fa3c866a7b6a6
Victor Cuerdo <vcuerdo@uninorte.edu.co>
2023-03-13 10:36:00 -0500

    edit profile button updated
    Se actualiza el boton de editar perfil de usuario,

--- lib/UI/profile_page.dart
+++ lib/UI/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'edit_profile_page.dart';
 import 'dart:math' as math;
 
 class Profile extends StatelessWidget {
@@ -16,8 +17,14 @@ class Profile extends StatelessWidget {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: const <Widget>[
-                Text('Tania', style: TextStyle(color: Colors.white, fontSize: 30),),
-                Text('Barranquilla, Atlantico', style: TextStyle(color: Colors.white, fontSize: 20),),
+                Text(
+                  'Tania',
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+                Text(
+                  'Barranquilla, Atlantico',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
               ],
             ),
             const CircleAvatar(
@@ -39,12 +46,24 @@ class Profile extends StatelessWidget {
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const EditarInfo()));
+                },
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: const <Widget>[
-                    Text('Edit Profile', style: TextStyle(color: Colors.white, fontSize: 20),),
-                    Icon(Icons.edit, color: Colors.white, size: 40,),
+                    Text(
+                      'Edit Profile',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Icon(
+                      Icons.edit,
+                      color: Colors.white,
+                      size: 40,
+                    ),
                     //SizedBox(width: 10),
                   ],
                 ),
@@ -62,8 +81,15 @@ class Profile extends StatelessWidget {
               onPressed: () {},
               child: Row(
                 children: const <Widget>[
-                  Text('Log out', style: TextStyle(color: Colors.red, fontSize: 20),),
-                  Icon(Icons.logout, color: Colors.red, size: 40,),
+                  Text(
+                    'Log out',
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  Icon(
+                    Icons.logout,
+                    color: Colors.red,
+                    size: 40,
+                  ),
                   //SizedBox(width: 10),
                 ],
               ),
@@ -72,20 +98,27 @@ class Profile extends StatelessWidget {
         ),
         const Text(''),
         const Text(''),
-        const Text('Statistics', style: TextStyle(color: Colors.white, fontSize: 18),),
+        const Text(
+          'Statistics',
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
         const Statistics(),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            const Text('Last Activity', style: TextStyle(color: Colors.white, fontSize: 15),),
+            const Text(
+              'Last Activity',
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
             TextButton(
               onPressed: () {},
               child: const Text(
                 'View all',
                 style: TextStyle(
-                  color: Colors.white,
-                  decoration: TextDecoration.underline,
-                  fontSize: 15),),
+                    color: Colors.white,
+                    decoration: TextDecoration.underline,
+                    fontSize: 15),
+              ),
             ),
           ],
         ),
@@ -104,7 +137,11 @@ class BackButton extends StatelessWidget {
       angle: 180 * math.pi / 180,
       child: IconButton(
         onPressed: () {},
-        icon: const Icon(Icons.logout, color: Colors.white, size: 20,),
+        icon: const Icon(
+          Icons.logout,
+          color: Colors.white,
+          size: 20,
+        ),
       ),
     );
   }
@@ -116,62 +153,85 @@ class Statistics extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [
-            Color(0xFF1C5E85),
-            Color(0xF05D8FAD),
-          ]
-        )
-      ),
-      child: OutlinedButton(
-        style: OutlinedButton.styleFrom(
-          shape: const RoundedRectangleBorder(
+        decoration: const BoxDecoration(
+            shape: BoxShape.rectangle,
             borderRadius: BorderRadius.all(Radius.circular(10)),
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color(0xFF1C5E85),
+                  Color(0xF05D8FAD),
+                ])),
+        child: OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
           ),
-        ),
-        onPressed: () {},
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              //Padding(padding: const EdgeInsets.symmetric(vertical: 10),
+          onPressed: () {},
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                //Padding(padding: const EdgeInsets.symmetric(vertical: 10),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const <Widget>[
-                    Padding(padding: EdgeInsets.only(top: 5, bottom: 10),
-                      child: Text('Cycling', style: TextStyle(color: Color(0xFFADCEE1)),),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5, bottom: 10),
+                      child: Text(
+                        'Cycling',
+                        style: TextStyle(color: Color(0xFFADCEE1)),
+                      ),
                     ),
-                    Text('Best time: 40:00', style: TextStyle(color: Colors.white, fontSize: 20),),
-                    Text('Distance: 20 km', style: TextStyle(color: Colors.white, fontSize: 20),),
-                    Text('Date: March 8, 2022', style: TextStyle(color: Colors.white, fontSize: 20),),
+                    Text(
+                      'Best time: 40:00',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Text(
+                      'Distance: 20 km',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Text(
+                      'Date: March 8, 2022',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
                   ],
                 ),
-              //Padding(padding: const EdgeInsets.symmetric(vertical: 10),
+                //Padding(padding: const EdgeInsets.symmetric(vertical: 10),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const <Widget>[
-                    Padding(padding: EdgeInsets.only(top: 5, bottom: 10),
-                      child: Text('Running', style: TextStyle(color: Color(0xFFADCEE1)),),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5, bottom: 10),
+                      child: Text(
+                        'Running',
+                        style: TextStyle(color: Color(0xFFADCEE1)),
+                      ),
                     ),
-                    Text('Best time: 40:00', style: TextStyle(color: Colors.white, fontSize: 20),),
-                    Text('Distance: 20 km', style: TextStyle(color: Colors.white, fontSize: 20),),
-                    Text('Date: March 8, 2022', style: TextStyle(color: Colors.white, fontSize: 20),),
+                    Text(
+                      'Best time: 40:00',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Text(
+                      'Distance: 20 km',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    Text(
+                      'Date: March 8, 2022',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
                   ],
                 ),
-            ],
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
 
@@ -181,50 +241,61 @@ class LastActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [
-            Color(0xFF1C5E85),
-            Color(0xF05D8FAD),
-          ]
-        )
-      ),
-      child: OutlinedButton(
-        style: OutlinedButton.styleFrom(
-          shape: const RoundedRectangleBorder(
+        decoration: const BoxDecoration(
+            shape: BoxShape.rectangle,
             borderRadius: BorderRadius.all(Radius.circular(10)),
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color(0xFF1C5E85),
+                  Color(0xF05D8FAD),
+                ])),
+        child: OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
           ),
-        ),
-        onPressed: () {},
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Padding(padding: const EdgeInsets.only(top: 10, bottom: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const <Widget>[
-                    Text('10 May, 12:30 pm', style: TextStyle(color: Colors.white, fontSize: 10),),
-                    Icon(Icons.settings, color: Colors.white, size: 20,),
-                  ],
+          onPressed: () {},
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const <Widget>[
+                        Text(
+                          '10 May, 12:30 pm',
+                          style: TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                        Icon(
+                          Icons.settings,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ],
+                    )),
+                const Padding(
+                    padding: EdgeInsets.only(top: 5, bottom: 5),
+                    child: Text(
+                      'Route 20: 40 km',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                const Padding(
+                  padding: EdgeInsets.only(top: 5, bottom: 10),
+                  child: Text(
+                    'Cycling',
+                    style: TextStyle(color: Color(0xFFADCEE1)),
+                  ),
                 )
-              ),
-              const Padding(padding: EdgeInsets.only(top: 5, bottom: 5),
-                child: Text('Route 20: 40 km', style: TextStyle(color: Colors.white),)
-              ),
-              const Padding(padding: EdgeInsets.only(top: 5, bottom: 10),
-                child: Text('Cycling', style: TextStyle(color: Color(0xFFADCEE1)),),
-              )
-            ],
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
