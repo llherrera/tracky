6734dff7df77f372fb63bd6c72f0e05830da60ab
Victor Cuerdo <vcuerdo@uninorte.edu.co>
2023-04-17 05:56:06 -0500

    widget_tests

---
+++ test/widget_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:tracky/Data/activity.dart';
+import 'package:flutter/material.dart';
+import 'package:tracky/Data/user.dart';
+import 'package:tracky/UI/activity_page.dart';
+import 'package:tracky/Widgets/home_widgets/lastActivity_button.dart';
+import 'package:tracky/UI/activity_detail.dart';
+import 'package:tracky/Widgets/segments_widget/list_segment.dart';
+
+void main() {
+  group('Widget Test para actidad', () {
+    testWidgets('1) Crear Actividad', (WidgetTester tester) async {});
+    testWidgets(
+        '2) Listar todas las actividades', (WidgetTester tester) async {});
+    testWidgets('3) Ver detalles actividad', (WidgetTester tester) async {});
+    testWidgets(
+        '4) Crear actividad multisusuarios', (WidgetTester tester) async {});
+    testWidgets('5) Guardar actividad', (WidgetTester tester) async {});
+  });
+}
